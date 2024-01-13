99778198c9aa99c8d14888b30929f5cbf17c5a66
LED <vmcalderingamez@gmail.com>
2024-01-13 14:57:07 -0500

    operators

---
+++ operators.js
@@ -0,0 +1,23 @@
+// Comparison Operators
+// < <= > >= == !=
+
+console.log("comparison operators: \n");
+
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log();
+console.log("A" == "B");
+console.log("A" != "B");
+
+// Logical Operators
+// && ||
+
+console.log("\nlogical operators: \n");
+
+console.log(10 == 10 && "A" == "A");
+console.log(10 == 10 && "A" == "B");
+console.log(10 == 10 || "A" == "B");
