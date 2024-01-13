ef20bb1c9aeb09f2584787ccff9f43dde5c37bed
LED <vmcalderingamez@gmail.com>
2024-01-13 00:38:26 -0500

    booleans

---
+++ booleans.js
@@ -0,0 +1,20 @@
+//Boolean
+
+var isAdult = true;
+
+console.log(isAdult);
+
+if (isAdult) {
+    console.log("is adult :)");
+} else {
+    console.log("is not adult :(");
+}
+
+console.log(!true);
+console.log(!false);
+
+if (!isAdult) {
+    console.log("is not adult :(");
+} else {
+    console.log("is adult :)");
+}
