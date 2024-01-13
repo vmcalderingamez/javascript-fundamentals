c0ccd742121f67861977f737e557bd3f59c33edb
LED <vmcalderingamez@gmail.com>
2024-01-12 23:59:08 -0500

    variables

--- hello-world.js
+++ hello-world.js
@@ -1,11 +1,2 @@
 console.log("Hello World Learning JS")
-// this will print the output
 console.log(10+10)
-
-/*
-lskdjflasjfsdfd
-fss
-agasgsg
-asggsdgsgasdg
-dk
-*/ 
---
+++ variables.js
@@ -0,0 +1,15 @@
+var firstname = "Jamila";
+var age = 21;
+var isFemale = true;
+var balance = 100.32;
+var dob = new Date(2000,0,30);
+var person = {};
+var empty = undefined;
+
+console.log(typeof firstname);
+console.log(typeof age);
+console.log(typeof isFemale);
+console.log(typeof balance);
+console.log(typeof dob);
+console.log(typeof person);
+console.log(typeof empty);
