fc19b8cc971366a37814bdbde461be73a94d1050
LED <vmcalderingamez@gmail.com>
2024-01-14 19:57:30 -0500

    type coercion

---
+++ type-coercion.js
@@ -0,0 +1,17 @@
+// with Type Coercion
+
+console.log(0 == false);
+console.log(0 == true);
+console.log("0" == true);
+console.log("0" == true);
+console.log("1" == true);
+
+// === without Type Coercion
+
+console.log();
+
+console.log(0 === false);
+console.log(0 === true);
+console.log("0" === true);
+console.log("0" === true);
+console.log("1" === true);
