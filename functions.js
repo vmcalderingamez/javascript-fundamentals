6ca908f6303e2c08e524d11adbda00afc47252de
LED <vmcalderingamez@gmail.com>
2024-01-14 21:23:12 -0500

    functions best practices & arrow functions

--- functions.js
+++ functions.js
@@ -24,3 +24,48 @@ addSpecifyedNumbers(10,11);
 var result = addSpecifyedNumbersAsResult(30, 5);
 
 console.log(result);
+
+// Best practice
+
+const getBrand = function (brand) {
+    return {
+        brand: brand,
+        website: `www.${brand}.com`
+    }
+}
+
+const brand = getBrand("Nike");
+console.log(JSON.stringify(brand,null,1));
+
+
+// now with arrows
+
+const getBrandv2 = brandv2 => ({
+        brandv2: brandv2,
+        website: `www.${brandv2}.com`
+    }
+)
+
+const add  = (a,b) => a + b;
+
+const calculate = (a,b,sign) => {
+    switch (sign) {
+        case "+":
+            return a + b;
+            break;
+        case "-":
+            return a - b;
+            break
+        default:
+            //throw an error
+            break;
+    }
+}
+
+// function default parameters
+
+const getBrandv3 = (brandv3 = "Hello") => ({
+    brandv3: brandv3,
+    website: `www.${brandv3}.com`
+}
+)
