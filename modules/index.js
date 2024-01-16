abe5faec44238d5c40e3e3e1160f66982aca16a1
LED <vmcalderingamez@gmail.com>
2024-01-15 22:05:27 -0500

    all the odds with imports & exports

--- modules/index.js
+++ modules/index.js
@@ -1,3 +1,10 @@
-import brand from "./lib.js"
+import 
+    brand,
+    // { add, substract }
+    * as lib
+from "./lib.js"
 
 console.log(brand);
+console.log(lib.brand);
+console.log(lib.add(10, 10));
+console.log(lib.substract(10, 10));
--- modules/lib.js
+++ modules/lib.js
@@ -3,4 +3,15 @@ const BRAND = {
     website: "amigoscode.com"
 };
 
+// export const add = (a, b) => a + b;
+// export const substract = (a, b) => a - b;
+
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+
+export {
+    add,
+    substract,
+    BRAND as brand
+}
 export default BRAND;
