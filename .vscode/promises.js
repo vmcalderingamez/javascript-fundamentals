405b78fae4421bf6a1671f1cac47fd2cb9a122f7
LED <vmcalderingamez@gmail.com>
2024-01-16 17:52:27 -0500

    async & await

--- .vscode/promises.js
+++ .vscode/promises.js
@@ -1,28 +1,52 @@
-// Promises 
-console.log("synchronous 1");
-const promise = new Promise((resolve, reject) => {
+// Async Await 
+
+const customers = new Promise((resolve, reject) => {
 
     return Promise.resolve().then(() => {
         let i = 0;
-        while (i < 1_000_000_000) {i++};
-        resolve("data back from the server");
+        while (i < 2_000_000_000) i++;
+        resolve([
+            {name: "Jamila", id: 1},
+            {name: "Bob", id: 1}
+        ]);
     });        
-
 });
 
-console.log("synchronous 2");
+const addresses = new Promise((resolve, reject) => {
 
-// const promise = new Promise((resolve, regect) => {
-//     setTimeout(() => {
-//         resolve("data back from the server");
-//     }, 3000);
+    return Promise.resolve().then(() => {
+        reject("oops error")
+        let i = 0;
+        while (i < 2_000_000_000) i++;
+        resolve([
+            {customerId: "1", address: "London"},
+            {customerId: "2", address: "USA"}
+        ]);
+    });        
+});
 
-//     setTimeout(() => {
-//         regect("falied to get data from the server");
-//     }, 5000);
-// });
+// const fetchData = () => {
+//     customers.then(c => {
+//         addresses.then(a => {
+//             console.log(a);
+//             console.log(c);
+//         }).catch(err => {
+//             console.log(err);
+//         })
+//     }).catch(err => {
+//         console.log(err);
+//     })
+// }
 
-promise.then(res => {
-    console.log(res);
-}).catch(err => console.log(err))
-.finally(() => console.log("done")); 
+const fetchData = async() => {
+    try {
+        const c = await customers;
+        const a = await addresses;
+        console.log(c);
+        console.log(a);            
+    } catch (error) {
+        console.log(error);
+    }    
+}
+
+fetchData();
