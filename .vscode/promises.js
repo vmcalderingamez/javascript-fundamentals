3d9b0426a69f566e74af17c70341a69895de8912
LED <vmcalderingamez@gmail.com>
2024-01-16 12:27:26 -0500

    synchronous promises

--- .vscode/promises.js
+++ .vscode/promises.js
@@ -1,14 +1,26 @@
 // Promises 
+console.log("synchronous 1");
+const promise = new Promise((resolve, reject) => {
 
-const promise = new Promise((resolve, regect) => {
-    setTimeout(() => {
+    return Promise.resolve().then(() => {
+        let i = 0;
+        while (i < 1_000_000_000) {i++};
         resolve("data back from the server");
-    }, 3000);
+    });        
 
-    setTimeout(() => {
-        regect("falied to get data from the server");
-    }, 5000);
 });
+
+console.log("synchronous 2");
+
+// const promise = new Promise((resolve, regect) => {
+//     setTimeout(() => {
+//         resolve("data back from the server");
+//     }, 3000);
+
+//     setTimeout(() => {
+//         regect("falied to get data from the server");
+//     }, 5000);
+// });
 
 promise.then(res => {
     console.log(res);
